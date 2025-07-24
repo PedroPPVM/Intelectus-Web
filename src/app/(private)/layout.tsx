49038e94f17b573bc95b4d5b1480435ef761353c
Pedro Paulo <pedro.paulo@agilean.com.br>
2025-07-24 16:12:43 -0300

    Feat: Added title, description and favicon to the head application

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,48 +1,21 @@
-'use client';
-
 import '../globals.css';
-import Header from '@/components/header';
-import { ThemeProvider } from '@/components/theme-provider';
-import { SidebarProvider } from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/app-sidebar';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-import { AuthProvider } from '@/contexts/AuthContext';
-import { Toaster } from '@/components/ui/sonner';
+import { Metadata } from 'next';
+import Providers from './providers';
+
+export const metadata: Metadata = {
+  title: 'Intelectus',
+  description: 'Sistema de Automação de Dados Públicos',
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <html lang="en">
       <body>
-        <AuthProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <QueryClientProvider client={queryClient}>
-              <SidebarProvider>
-                <AppSidebar />
-                <div className="flex min-h-screen w-full flex-col antialiased">
-                  <Header />
-
-                  <div className="flex flex-1 flex-col gap-4 p-6">
-                    {children}
-                  </div>
-
-                  <Toaster richColors />
-                </div>
-              </SidebarProvider>
-            </QueryClientProvider>
-          </ThemeProvider>
-        </AuthProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
--- src/app/(public)/layout.tsx
+++ src/app/(public)/layout.tsx
@@ -1,36 +1,21 @@
-'use client';
-
 import '../globals.css';
-import { ThemeProvider } from '@/components/theme-provider';
-import { AuthProvider } from '@/contexts/AuthContext';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from 'react';
-import { Toaster } from 'sonner';
+import { Metadata } from 'next';
+import Providers from './providers';
+
+export const metadata: Metadata = {
+  title: 'Intelectus',
+  description: 'Sistema de Automação de Dados Públicos',
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [queryClient] = useState(() => new QueryClient());
-
   return (
     <html lang="en">
-      <body className="flex min-h-screen flex-col antialiased">
-        <AuthProvider>
-          <QueryClientProvider client={queryClient}>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="system"
-              enableSystem
-              disableTransitionOnChange
-            >
-              {children}
-
-              <Toaster richColors />
-            </ThemeProvider>
-          </QueryClientProvider>
-        </AuthProvider>
+      <body>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
