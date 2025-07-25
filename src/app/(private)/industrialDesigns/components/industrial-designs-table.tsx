bfca864676eedf29c28fd6b1c42329dbe64cea7d
Pedro Paulo <pedro.paulo@agilean.com.br>
2025-07-25 12:16:10 -0300

    Fix: Fixed all table overflow to scroll

--- src/app/(private)/industrialDesigns/components/industrial-designs-table.tsx
+++ src/app/(private)/industrialDesigns/components/industrial-designs-table.tsx
@@ -124,7 +124,7 @@ const IndustrialDesignsTable = ({
   }, [processIdToDelete]);
 
   return (
-    <div className="max-h-full overflow-auto">
+    <div className="flex max-h-[calc(100vh-224px)] max-w-[calc(100vw-354px)] overflow-auto">
       <Table>
         <TableHeader>
           <TableRow className="hover:bg-transparent">
